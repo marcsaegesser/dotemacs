6993e17c31ab77ea2ecf44802f0fb4392b1d4a54
Marc A. Saegesser <msaegesser@amientertainment.com>
2018-02-08 19:06:43 -0600

    Haskell

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -1,32 +1,147 @@
-(require-package 'haskell-mode)
+(use-package flycheck-haskell
+  :ensure t
+  :after haskell-mode
+  :config
+  (flycheck-haskell-setup))
 
-(dolist (hook '(haskell-mode-hook inferior-haskell-mode-hook))
-  (add-hook hook 'turn-on-haskell-doc-mode))
+(use-package haskell-mode
+  :ensure t
+  :mode (("\\.hs\\(c\\|-boot\\)?\\'" . haskell-mode)
+         ("\\.lhs\\'" . literate-haskell-mode)
+         ("\\.cabal\\'" . haskell-cabal-mode))
+  :bind (:map haskell-mode-map
+              ("C-c C-h" . my-haskell-hoogle)
+              ("C-c C-," . haskell-navigate-imports)
+              ("C-c C-." . haskell-mode-format-imports)
+              ("C-c C-u" . my-haskell-insert-undefined)
+              ("M-s")
+              ("M-t"))
+  :preface
+  (defun my-haskell-insert-undefined ()
+    (interactive) (insert "undefined"))
 
-(add-auto-mode 'haskell-mode "\\.ghci\\'")
+  (defun snippet (name)
+    (interactive "sName: ")
+    (find-file (expand-file-name (concat name ".hs") "~/src/notes"))
+    (haskell-mode)
+    (goto-char (point-min))
+    (when (eobp)
+      (insert "hdr")
+      (yas-expand)))
 
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-(add-hook 'haskell-mode-hook (lambda () (subword-mode +1)))
+  (defvar hoogle-server-process nil)
+  (defun my-haskell-hoogle (query &optional arg)
+    "Do a Hoogle search for QUERY."
+    (interactive
+     (let ((def (haskell-ident-at-point)))
+       (if (and def (symbolp def)) (setq def (symbol-name def)))
+       (list (read-string (if def
+                              (format "Hoogle query (default %s): " def)
+                            "Hoogle query: ")
+                          nil nil def)
+             current-prefix-arg)))
+    (unless (and hoogle-server-process
+                 (process-live-p hoogle-server-process))
+      (message "Starting local Hoogle server on port 8687...")
+      (with-current-buffer (get-buffer-create " *hoogle-web*")
+        (cd temporary-file-directory)
+        (setq hoogle-server-process
+              (start-process "hoogle-web" (current-buffer) "hoogle"
+                             "server" "--local" "--port=8687")))
+      (message "Starting local Hoogle server on port 8687...done"))
+    (browse-url
+     (format "http://127.0.0.1:8687/?hoogle=%s"
+             (replace-regexp-in-string
+              " " "+" (replace-regexp-in-string "\\+" "%2B" query)))))
 
-(eval-after-load 'haskell-mode
-  '(progn
-     (define-key haskell-mode-map (kbd "C-c h") 'hoogle)))
+  (defvar haskell-prettify-symbols-alist
+    '(("::"     . ?∷)
+      ("forall" . ?∀)
+      ("exists" . ?∃)
+      ("->"     . ?→)
+      ("<-"     . ?←)
+      ("=>"     . ?⇒)
+      ("~>"     . ?⇝)
+      ("<~"     . ?⇜)
+      ("<>"     . ?⨂)
+      ("msum"   . ?⨁)
+      ("\\"     . ?λ)
+      ("not"    . ?¬)
+      ("&&"     . ?∧)
+      ("||"     . ?∨)
+      ("/="     . ?≠)
+      ("<="     . ?≤)
+      (">="     . ?≥)
+      ("<<<"    . ?⋘)
+      (">>>"    . ?⋙)
 
-(when (eval-when-compile (>= emacs-major-version 24))
-  (require-package 'ghci-completion)
-  (add-hook 'inferior-haskell-mode-hook 'turn-on-ghci-completion))
+      ("`elem`"             . ?∈)
+      ("`notElem`"          . ?∉)
+      ("`member`"           . ?∈)
+      ("`notMember`"        . ?∉)
+      ("`union`"            . ?∪)
+      ("`intersection`"     . ?∩)
+      ("`isSubsetOf`"       . ?⊆)
+      ("`isProperSubsetOf`" . ?⊂)
+      ("undefined"          . ?⊥)))
 
-(require-package 'flymake-haskell-multi)
-(add-hook 'haskell-mode-hook #'flymake-haskell-multi-load)
+  :config
+  (require 'haskell)
+  (require 'haskell-doc)
 
-;; Make compilation-mode understand "at blah.hs:11:34-50" lines output by GHC
-(eval-after-load 'compile
-  '(progn
-     (let ((alias 'ghc-at-regexp))
-       (add-to-list
-        'compilation-error-regexp-alist-alist
-        (list alias " at \\(.*l?hs\\):\\([0-9]+\\):\\([0-9]+\\)-[0-9]+$" 1 2 3 0 1))
-       (add-to-list
-        'compilation-error-regexp-alist alias))))
+  (defun my-haskell-mode-hook ()
+    (haskell-indentation-mode)
+    (interactive-haskell-mode)
+    (diminish 'interactive-haskell-mode)
+    (flycheck-mode 1)
+    (setq-local prettify-symbols-alist haskell-prettify-symbols-alist)
+    (prettify-symbols-mode 1)
+    (bug-reference-prog-mode 1))
+
+  (add-hook 'haskell-mode-hook 'my-haskell-mode-hook)
+
+  (eval-after-load 'align
+    '(nconc
+      align-rules-list
+      (mapcar #'(lambda (x)
+                  `(,(car x)
+                    (regexp . ,(cdr x))
+                    (modes quote (haskell-mode literate-haskell-mode))))
+              '((haskell-types       . "\\(\\s-+\\)\\(::\\|∷\\)\\s-+")
+                (haskell-assignment  . "\\(\\s-+\\)=\\s-+")
+                (haskell-arrows      . "\\(\\s-+\\)\\(->\\|→\\)\\s-+")
+                (haskell-left-arrows . "\\(\\s-+\\)\\(<-\\|←\\)\\s-+"))))))
+
+
+;; (require-package 'haskell-mode)
+
+;; (dolist (hook '(haskell-mode-hook inferior-haskell-mode-hook))
+;;   (add-hook hook 'turn-on-haskell-doc-mode))
+
+;; (add-auto-mode 'haskell-mode "\\.ghci\\'")
+
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;; (add-hook 'haskell-mode-hook (lambda () (subword-mode +1)))
+
+;; (eval-after-load 'haskell-mode
+;;   '(progn
+;;      (define-key haskell-mode-map (kbd "C-c h") 'hoogle)))
+
+;; (when (eval-when-compile (>= emacs-major-version 24))
+;;   (require-package 'ghci-completion)
+;;   (add-hook 'inferior-haskell-mode-hook 'turn-on-ghci-completion))
+
+;; (require-package 'flymake-haskell-multi)
+;; (add-hook 'haskell-mode-hook #'flymake-haskell-multi-load)
+
+;; ;; Make compilation-mode understand "at blah.hs:11:34-50" lines output by GHC
+;; (eval-after-load 'compile
+;;   '(progn
+;;      (let ((alias 'ghc-at-regexp))
+;;        (add-to-list
+;;         'compilation-error-regexp-alist-alist
+;;         (list alias " at \\(.*l?hs\\):\\([0-9]+\\):\\([0-9]+\\)-[0-9]+$" 1 2 3 0 1))
+;;        (add-to-list
+;;         'compilation-error-regexp-alist alias))))
 
 (provide 'init-haskell)
