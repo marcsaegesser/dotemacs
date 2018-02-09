1b498da066e96e14c89ca765b9b80ecc27b7fee9
Marc A. Saegesser <msaegesser@amientertainment.com>
2018-02-08 18:56:42 -0600

    Python, css, nxml

--- lisp/init-nxml.el
+++ lisp/init-nxml.el
@@ -1,40 +1,70 @@
-(add-auto-mode
- 'nxml-mode
- (concat "\\."
-         (regexp-opt
-          '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss"
-            "gpx" "tcx"))
-         "\\'"))
-(setq magic-mode-alist (cons '("<\\?xml " . nxml-mode) magic-mode-alist))
-(fset 'xml-mode 'nxml-mode)
-(add-hook 'nxml-mode-hook (lambda ()
-                            (set (make-local-variable 'ido-use-filename-at-point) nil)))
-(setq nxml-slash-auto-complete-flag t)
+(use-package nxml-mode
+  :commands nxml-mode
+  :bind (:map nxml-mode-map
+              ("<return>" . newline-and-indent)
+              ("C-c M-h"  . tidy-xml-buffer))
+  :preface
+  (defun tidy-xml-buffer ()
+    (interactive)
+    (save-excursion
+      (call-process-region (point-min) (point-max) "tidy" t t nil
+                           "-xml" "-i" "-wrap" "0" "-omit" "-q" "-utf8")))
+  :init
+  (defalias 'xml-mode 'nxml-mode)
+  :config
+  (autoload 'sgml-skip-tag-forward "sgml-mode")
+  (add-to-list 'hs-special-modes-alist
+               '(nxml-mode
+                 "<!--\\|<[^/>]*[^/]>"
+                 "-->\\|</[^/>]*[^/]>"
+                 "<!--"
+                 sgml-skip-tag-forward
+                 nil)))
+
+(use-package tidy
+  :ensure t
+  :commands (tidy-buffer
+             tidy-parse-config-file
+             tidy-save-settings
+             tidy-describe-options))
+
+;; (add-auto-mode
+;;  'nxml-mode
+;;  (concat "\\."
+;;          (regexp-opt
+;;           '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss"
+;;             "gpx" "tcx"))
+;;          "\\'"))
+;; (setq magic-mode-alist (cons '("<\\?xml " . nxml-mode) magic-mode-alist))
+;; (fset 'xml-mode 'nxml-mode)
+;; (add-hook 'nxml-mode-hook (lambda ()
+;;                             (set (make-local-variable 'ido-use-filename-at-point) nil)))
+;; (setq nxml-slash-auto-complete-flag t)
 
 
 ;; See: http://sinewalker.wordpress.com/2008/06/26/pretty-printing-xml-with-emacs-nxml-mode/
-(defun pp-xml-region (begin end)
-  "Pretty format XML markup in region. The function inserts
-linebreaks to separate tags that have nothing but whitespace
-between them.  It then indents the markup by using nxml's
-indentation rules."
-  (interactive "r")
-  (save-excursion
-      (nxml-mode)
-      (goto-char begin)
-      (while (search-forward-regexp "\>[ \\t]*\<" nil t)
-        (backward-char) (insert "\n"))
-      (indent-region begin end)))
+;; (defun pp-xml-region (begin end)
+;;   "Pretty format XML markup in region. The function inserts
+;; linebreaks to separate tags that have nothing but whitespace
+;; between them.  It then indents the markup by using nxml's
+;; indentation rules."
+;;   (interactive "r")
+;;   (save-excursion
+;;       (nxml-mode)
+;;       (goto-char begin)
+;;       (while (search-forward-regexp "\>[ \\t]*\<" nil t)
+;;         (backward-char) (insert "\n"))
+;;       (indent-region begin end)))
 
 ;;----------------------------------------------------------------------------
 ;; Integration with tidy for html + xml
 ;;----------------------------------------------------------------------------
-(require-package 'tidy)
-(add-hook 'nxml-mode-hook (lambda () (tidy-build-menu nxml-mode-map)))
-(add-hook 'html-mode-hook (lambda () (tidy-build-menu html-mode-map)))
+;; (require-package 'tidy)
+;; (add-hook 'nxml-mode-hook (lambda () (tidy-build-menu nxml-mode-map)))
+;; (add-hook 'html-mode-hook (lambda () (tidy-build-menu html-mode-map)))
 
 
-(add-auto-mode 'html-mode "\\.(jsp|tmpl)\\'")
+;; (add-auto-mode 'html-mode "\\.(jsp|tmpl)\\'")
 
 
 (provide 'init-nxml)
--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -1,18 +1,49 @@
+(use-package python-mode
+  :mode "\\.py\\'"
+  :interpreter "python"
+  :bind (:map python-mode-map
+              ("C-c c")
+              ("C-c C-z" . python-shell))
+  :config
+  (defvar python-mode-initialized nil)
+
+  (defun my-python-mode-hook ()
+    (unless python-mode-initialized
+      (setq python-mode-initialized t)
+
+      (info-lookup-add-help
+       :mode 'python-mode
+       :regexp "[a-zA-Z_0-9.]+"
+       :doc-spec
+       '(("(python)Python Module Index" )
+         ("(python)Index"
+          (lambda
+            (item)
+            (cond
+             ((string-match
+               "\\([A-Za-z0-9_]+\\)() (in module \\([A-Za-z0-9_.]+\\))" item)
+              (format "%s.%s" (match-string 2 item)
+                      (match-string 1 item)))))))))
+
+    (set (make-local-variable 'parens-require-spaces) nil)
+    (setq indent-tabs-mode nil))
+
+  (add-hook 'python-mode-hook 'my-python-mode-hook))
 
 
-(autoload 'doctest-mode "doctest-mode" "Python doctest editing mode." t)
+;; (autoload 'doctest-mode "doctest-mode" "Python doctest editing mode." t)
 
-(setq auto-mode-alist
-      (append '(("SConstruct\\'" . python-mode)
-		("SConscript\\'" . python-mode))
-              auto-mode-alist))
+;; (setq auto-mode-alist
+;;       (append '(("SConstruct\\'" . python-mode)
+;; 		("SConscript\\'" . python-mode))
+;;               auto-mode-alist))
 
 
-;;----------------------------------------------------------------------------
-;; On-the-fly syntax checking via flymake
-;;----------------------------------------------------------------------------
-(require-package 'flymake-python-pyflakes)
-(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+;; ;;----------------------------------------------------------------------------
+;; ;; On-the-fly syntax checking via flymake
+;; ;;----------------------------------------------------------------------------
+;; (require-package 'flymake-python-pyflakes)
+;; (add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
 
 
 (provide 'init-python-mode)
